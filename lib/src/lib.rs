e07a23b634f42f3a3544afc610e78e9af1ae651d
UselessStar <drvoXs@gmail.com>
2026-09-15 15:19:36 -0300

    feat(lib): add name field and method

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -13,20 +13,34 @@
 use uuid::Uuid;
 
 /// todo
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Task {
     id: Uuid,
+    name: String,
 }
 
 impl Task {
     /// todo
-    pub fn new(id: impl Into<Uuid>) -> Task {
-        Self { id: id.into() }
+    pub fn new(id: impl Into<Uuid>, name: impl Into<String>) -> Task {
+        Self {
+            id: id.into(),
+            name: name.into(),
+        }
     }
 
     /// todo
     pub fn id(&self) -> Uuid {
         self.id
+    }
+
+    /// todo
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// todo
+    pub fn set_name(&mut self, name: impl Into<String>) {
+        self.name = name.into();
     }
 }
