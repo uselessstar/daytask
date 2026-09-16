e0b0aca8852b533ebb58f60cc8606621068a5431
UselessStar <drvoXs@gmail.com>
2026-09-15 23:45:22 -0300

    feat(lib): implement TaskBuilder for constructing Task instances

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -13,6 +13,40 @@
 use uuid::Uuid;
 
 /// todo
+#[derive(Debug, Default)]
+pub struct TaskBuilder {
+    name: String,
+    id: Option<Uuid>,
+}
+
+impl TaskBuilder {
+    /// todo
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// todo
+    pub fn with_id(mut self, id: impl Into<Uuid>) -> Self {
+        self.id = Some(id.into());
+        self
+    }
+
+    /// todo
+    pub fn with_name(mut self, name: impl Into<String>) -> Self {
+        self.name = name.into();
+        self
+    }
+
+    /// todo
+    pub fn build(self) -> Task {
+        Task {
+            id: self.id.unwrap_or_else(Uuid::now_v7),
+            name: self.name,
+        }
+    }
+}
+
+/// todo
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Task {
@@ -22,9 +56,9 @@ pub struct Task {
 
 impl Task {
     /// todo
-    pub fn new(id: impl Into<Uuid>, name: impl Into<String>) -> Task {
+    pub fn new(name: impl Into<String>) -> Self {
         Self {
-            id: id.into(),
+            id: Uuid::now_v7(),
             name: name.into(),
         }
     }
