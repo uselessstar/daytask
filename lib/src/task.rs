c8241c6aba74d8bfd8b0075dce5cb114730359cf
MatiasSagredo <ma.sagredom@duocuc.cl>
2026-09-17 09:07:28 -0300

    refaactor(lib): move structs and macros in its own modules

---
+++ lib/src/task.rs
@@ -0,0 +1,70 @@
+use uuid::Uuid;
+
+/// todo
+#[derive(Debug, Default)]
+pub struct TaskBuilder {
+    name: String,
+    id: Option<Uuid>,
+}
+
+impl TaskBuilder {
+    /// todo
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// todo
+    pub fn with_id(mut self, id: impl Into<Uuid>) -> Self {
+        self.id = Some(id.into());
+        self
+    }
+
+    /// todo
+    pub fn with_name(mut self, name: impl Into<String>) -> Self {
+        self.name = name.into();
+        self
+    }
+
+    /// todo
+    pub fn build(self) -> Task {
+        let id = self.id.unwrap_or_else(Uuid::now_v7);
+        debug_log!(target: "task", "Task built: id={}, name={:?}", id, self.name);
+        Task { id, name: self.name }
+    }
+}
+
+/// todo
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+pub struct Task {
+    id: Uuid,
+    name: String,
+}
+
+impl Task {
+    /// todo
+    pub fn new(name: impl Into<String>) -> Self {
+        let task = Self {
+            id: Uuid::now_v7(),
+            name: name.into(),
+        };
+        debug_log!(target: "task","Task created: {:#?}", task);
+        task
+    }
+
+    /// todo
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+
+    /// todo
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    /// todo
+    pub fn set_name(&mut self, name: impl Into<String>) {
+        let old = std::mem::replace(&mut self.name, name.into());
+        debug_log!(target: "task", "Task name changed: id={}, old={:?}, new={:?}", self.id, old, self.name);
+    }
+}
