c9e9a7630c0cdecf70a8fa03cad7fa476c2a8099
UselessStar <drvoXs@gmail.com>
2026-09-17 19:32:08 -0300

    feat(lib): add description field and methods to Task struct

--- lib/src/task.rs
+++ lib/src/task.rs
@@ -6,10 +6,11 @@ use uuid::Uuid;
 pub struct Task {
     id: Uuid,
     name: String,
+    description: Option<String>,
 }
 
 impl Task {
-    /// Creates a new [Task].
+    /// Creates a new [Task] with the specified name.
     ///
     /// # Arguments
     /// - **name**: The name to set to the [task](Task).
@@ -27,6 +28,7 @@ impl Task {
         let task = Self {
             id: Uuid::now_v7(),
             name: name.into(),
+            description: None,
         };
         debug_log!(target: "task","Task created: {:#?}", task);
         task
@@ -40,6 +42,11 @@ impl Task {
     /// Returns the name of the [task](Task).
     pub fn name(&self) -> &str {
         &self.name
+    }
+
+    /// Returns the description of the [task](Task).
+    pub fn description(&self) -> Option<&str> {
+        self.description.as_deref()
     }
 
     /// Sets the name of the [task](Task).
@@ -65,6 +72,56 @@ impl Task {
         #[cfg(not(feature = "log"))]
         {
             self.name = name.into();
+        }
+    }
+
+    /// Sets the description of the [task](Task).
+    ///
+    /// # Arguments
+    /// - **description**: The new description to set.
+    ///
+    /// # Example
+    /// ```
+    /// # use daytask::Task;
+    /// #
+    /// let mut task = Task::new("example");
+    /// task.set_description("This is a description");
+    ///
+    /// assert_eq!(task.description(), Some("This is a description"));
+    /// ```
+    pub fn set_description(&mut self, description: impl Into<String>) {
+        #[cfg(feature = "log")]
+        {
+            let old = self.description.replace(description.into());
+            debug_log!(target: "task", "Task description changed: id={}, old={:?}, new={:?}", self.id, old, self.description);
+        }
+        #[cfg(not(feature = "log"))]
+        {
+            self.description = Some(description.into());
+        }
+    }
+
+    /// Clears the description of the [task](Task).
+    ///
+    /// # Example
+    /// ```
+    /// # use daytask::Task;
+    /// #
+    /// let mut task = Task::new("example");
+    /// task.set_description("This is a description");
+    /// task.clear_description();
+    ///
+    /// assert_eq!(task.description(), None);
+    /// ```
+    pub fn clear_description(&mut self) {
+        #[cfg(feature = "log")]
+        {
+            let old = self.description.take();
+            debug_log!(target: "task", "Task description cleared: id={}, old={:?}", self.id, old);
+        }
+        #[cfg(not(feature = "log"))]
+        {
+            self.description = None;
         }
     }
 }
@@ -98,6 +155,45 @@ mod tests {
     }
 
     #[test]
+    fn test_description_starts_empty_and_can_be_set() {
+        let mut task = Task::new("Description Test");
+
+        assert_eq!(task.description(), None);
+
+        task.set_description("Task details");
+
+        assert_eq!(task.description(), Some("Task details"));
+    }
+
+    #[test]
+    fn test_description_can_be_replaced_and_removed() {
+        let mut task = Task::new("Description Update Test");
+
+        task.set_description("Initial details");
+        task.set_description(String::from("Updated details"));
+
+        assert_eq!(task.description(), Some("Updated details"));
+
+        task.clear_description();
+        assert_eq!(task.description(), None);
+
+        task.clear_description();
+        assert_eq!(task.description(), None);
+    }
+
+    #[test]
+    fn test_mutations_preserve_task_identity() {
+        let mut task = Task::new("Identity Test");
+        let id = task.id();
+
+        task.set_name("Renamed");
+        task.set_description("Details");
+        task.clear_description();
+
+        assert_eq!(task.id(), id);
+    }
+
+    #[test]
     fn test_equality_and_cloning() {
         let task = Task::new("Clone Test");
         let cloned_task = task.clone();
@@ -110,7 +206,8 @@ mod tests {
     #[cfg(feature = "serde")]
     #[test]
     fn test_serde_serialization() {
-        let task = Task::new("Serde Test");
+        let mut task = Task::new("Serde Test");
+        task.set_description("Serialized details");
         let serialized = serde_json::to_string(&task).expect("Serialization Failed");
         let deserialized: Task = serde_json::from_str(&serialized).expect("Deserialization Failed");
 
