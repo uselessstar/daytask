505ea698b81eef1e204bc596d54115d4e4fb7962
UselessStar <drvoXs@gmail.com>
2026-09-18 12:56:33 -0300

    feat(lib): update Clippy lints to include missing panics and safety documentation checks

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -7,7 +7,9 @@
         unused,
         clippy::perf,
         clippy::suspicious,
-        clippy::missing_errors_doc
+        clippy::missing_errors_doc,
+        clippy::missing_panics_doc,
+        clippy::missing_safety_doc
     )
 )]
 
