157d92f72fe9812c29a86478ed8af227f290e664
UselessStar <drvoXs@gmail.com>
2026-09-19 00:06:15 -0300

    feat(task): refactor Status enum and related implementations into a separate module

---
+++ lib/src/task/status.rs
@@ -0,0 +1,86 @@
+use crate::TaskError;
+use std::fmt::Display;
+
+use super::Result;
+
+/// Indicates the current status of a [task](Task).
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Default)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[repr(u8)]
+#[non_exhaustive]
+pub enum Status {
+    /// Pending status indicates that the task has not yet been started.
+    #[default]
+    Pending = 0,
+    /// In Progress status indicates that the task is currently being worked on.
+    InProgress = 1,
+    /// Completed status indicates that the task has been finished.
+    Completed = 2,
+}
+
+impl Status {
+    /// Returns `true` if the status is [Pending](Status::Pending), `false` otherwise.
+    #[must_use]
+    #[inline]
+    pub fn is_pending(&self) -> bool {
+        matches!(self, Self::Pending)
+    }
+
+    /// Returns `true` if the status is [InProgress](Status::InProgress), `false` otherwise.
+    #[inline]
+    #[must_use]
+    pub fn is_in_progress(&self) -> bool {
+        matches!(self, Self::InProgress)
+    }
+
+    /// Returns `true` if the status is [Completed](Status::Completed), `false` otherwise.
+    #[inline]
+    #[must_use]
+    pub fn is_completed(&self) -> bool {
+        matches!(self, Self::Completed)
+    }
+}
+
+impl TryFrom<u8> for Status {
+    type Error = TaskError;
+
+    #[inline]
+    fn try_from(value: u8) -> Result<Self> {
+        match value {
+            x if x == Self::Pending as u8 => Ok(Self::Pending),
+            x if x == Self::InProgress as u8 => Ok(Self::InProgress),
+            x if x == Self::Completed as u8 => Ok(Self::Completed),
+            _ => Err(TaskError::InvalidStatusValue(value)),
+        }
+    }
+}
+
+impl From<Status> for u8 {
+    #[inline]
+    fn from(status: Status) -> Self {
+        status as Self
+    }
+}
+
+impl Display for Status {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let status_str = match self {
+            Self::Pending => "Pending",
+            Self::InProgress => "In Progress",
+            Self::Completed => "Completed",
+        };
+        write!(f, "{}", status_str)
+    }
+}
+
+impl AsRef<str> for Status {
+    #[inline]
+    fn as_ref(&self) -> &str {
+        match self {
+            Self::Pending => "Pending",
+            Self::InProgress => "In Progress",
+            Self::Completed => "Completed",
+        }
+    }
+}
